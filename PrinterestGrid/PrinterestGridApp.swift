c6ef19170a417b87428abc11a28bc97356768725
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-11-21 17:30:49 +0500

    Initial Commit

---
+++ PrinterestGrid/PrinterestGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrinterestGridApp.swift
+//  PrinterestGrid
+//
+//  Created by Arkasha Zuev on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PrinterestGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
